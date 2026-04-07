c37ebd43189a440134663f0e8de06ab0da94da55
sakshinagare21 <sakshinagare2112@gmail.com>
2026-04-07 19:06:36 +0530

    Updated Firebase initialization to use environment variables for deployment

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,14 +1,30 @@
-import admin from "firebase-admin";
-import fs from "fs";
+// import admin from "firebase-admin";
+// import fs from "fs";
 
-const serviceAccount = JSON.parse(
- fs.readFileSync(new URL("./firebaseServiceKey.json", import.meta.url))
-);
+// const serviceAccount = JSON.parse(
+//  fs.readFileSync(new URL("./firebaseServiceKey.json", import.meta.url))
+// );
+
+// admin.initializeApp({
+//  credential: admin.credential.cert(serviceAccount),
+// });
+
+// console.log("Firebase initialized");
+
+// export default admin;
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+import admin from "firebase-admin";
+
+const serviceAccount = {
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
 
 admin.initializeApp({
- credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
-
-console.log("Firebase initialized");
+console.log("firebase initialization")
 
 export default admin;
